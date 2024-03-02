c379aa5fdbf1fc8540db3ccf49f601aea14b1eb4
Devottam Vaishnav <81460876+devottam123@users.noreply.github.com>
2024-03-02 19:40:25 +0000

    Day 5

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,64 @@
+const user = {
+    username: "Devottam",
+    price: 999,
+
+    welcomeMessage: function () {
+        console.log(`${this.username}, welcome to website.`);
+        console.log(this);
+    }    
+}
+
+// user.welcomeMessage()
+// user.username="sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+    // console.log(this);
+// }
+
+// chai()
+
+// this doesn't work in Basic function
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+    // console.log(this);
+// }
+
+// chai()
+
+// const chai = () => {
+//     let username = "hitesh"
+//     console.log(this.username);
+//     // console.log(this);
+// }
+
+// chai()
+
+
+// Explicit return type of arrow function
+// const addTwo = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+// console.log(addTwo(3, 4));
+
+// Implicit return type of arrow function
+// const addTwo = (num1, num2) =>  num1 + num2;
+// const addTwo = (num1, num2) =>  (num1 + num2);
+// console.log(addTwo(3, 4));
+
+
+// const addTwo = (num1, num2) =>  ({username: "hitesh"})
+// console.log(addTwo(3, 4));
+// For returning an object round brackets are compulsory.
+
+// const myArray = [2,5,3,7,8]
+// myArray.forEach(() => {
+    
+// })
---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expression
+// VVIP: Always stop a IIFE with a ';' as doesn't know where to stop it's execution.
+
+// Named IIFE, since it has a name 'chai'
+(function chai(){
+    console.log(`DB CONNECTED`);
+})();
+
+// Simple IIFE
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})('Devottam');
+
+// chai()
